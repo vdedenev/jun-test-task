01d8896e60315a58f1595348ba1e1e691e2a8e3a
vdedenev <v.s.dedenev@gmail.com>
2021-07-29 16:09:28 +0500

    react init

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap'
+import {Badge} from "react-bootstrap";
+
+function App() {
+  return (
+      <div className="container">
+        <h1> 123</h1>
+      </div>
+  )
+}
+
+export default App;
