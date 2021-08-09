d9c0974113a4f82dec299e0ee0d4b8928f7033bc
vdedenev <v.s.dedenev@gmail.com>
2021-08-09 12:32:06 +0500

    1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express =  require('express')
 const config = require('config')
 const typeorm = require('typeorm')
+const path = require("path");
 
 const app = express()
 
@@ -8,6 +9,15 @@ app.use(express.json())
 app.use('/', require('./routes/task'))
 app.use('/auth/', require('./routes/auth'))
 app.use('/user/', require('./routes/user'))
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('123')
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 //const PORT = config.get('port')
@@ -20,7 +30,7 @@ async function start(){
     catch (e) {
         throw e.message
     }
-    app.listen(process.env.PORT || 3000, () => console.log(`App has been started on port ${process.env.PORT}`))
+    app.listen(process.env.PORT || 3000, () => console.log(`App has been started on port ${process.env.PORT || 3000}`))
 }
 
 start()
